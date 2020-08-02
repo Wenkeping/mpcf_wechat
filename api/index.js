6ff7a1f87886b3d40c7e8982ededf578eb47487e
wenkeping <527999446@qq.com>
2020-08-02 18:51:44 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,33 +1,21 @@
 import categories from './categories.js'
 import orders from './orders.js'
 import storeOrders from './storeOrders.js'
-
-import mart from './mart.js'
-import productList from './productList.js'
-import notices from './notices.js'
-import productDetail from './productDetail.js'
 import scores from './scores.js'
-import martDetail from './martDetail.js'
 import hotSearch from './hotSearch.js'
 import historySearch from './historySearch.js'
 import orderDetail from './orderDetail.js'
-import giftCards from './giftCards.js'
+
 
 const json = {
 	categories,
 	orders,
 	storeOrders,
-
-	mart,
-	productList,
-	notices,
-	productDetail,
 	scores,
-	martDetail,
 	hotSearch,
 	historySearch,
-	orderDetail,
-	giftCards
+	orderDetail
+
 }
 
 export default (name, loading = true) => {
